16f9b13cbea847064f8ae0d8e770c5effee60d67
Cezar Castro Rosa <cecez86@gmail.com>
2022-02-01 15:17:33 -0300

    docs: mais anotações sobre unknown e casting

--- 005-laracasts-beggining-typescript/06-any_and_unknown.ts
+++ 005-laracasts-beggining-typescript/06-any_and_unknown.ts
@@ -30,9 +30,26 @@ logger3({"nao": "string"})
 
 type tipoAny = { prop1: any, prop2: any }
 const constAny: tipoAny = { prop1: "stringue", prop2: 123 }
-console.log(constAny.prop2.qualquerCoisaAquiPoisNaoTemChecagem.e.aqui.tambem)
+//console.log(constAny.prop2.qualquerCoisaAquiPoisNaoTemChecagem.e.aqui.tambem) <- erro na execução apenas
 
-// um pouco melhor com unknown
-type tipoUnknown = { prop1: unknown, prop2: unknown }
-const constUnknown: tipoUnknown = { prop1: "stringue", prop2: 123 }
-console.log(constUnknown.prop2.coco.coco)
+// forma "aceita" de utilizar any (fazendo mais checagens pelo código)
+type tipoUnknown = { prop1: unknown, prop2: any }
+const constUnknown: tipoUnknown = { prop1: "stringue", prop2: { "foo": "bar" } }
+// console.log(constUnknown.prop1.coco.coco)  <-- gera erro
+
+if (
+    constUnknown.prop2 && // verifica se não é undefined
+    typeof constUnknown.prop2 === 'object' && // verifica se é objeto
+    Object.hasOwnProperty.call(constUnknown.prop2, 'foo')
+) {
+    //console.log(constUnknown.prop2.foo);
+    
+    // casting
+    console.log(
+        (constUnknown.prop2 as {foo: unknown}).foo
+    )
+}
+
+// casting para tipos vindos do HTML (ex: HTMLElement, HTMLCanvasElement)
+// <canvas id="meu_canvas">
+const canvas = document.getElementById('meu_canvas') as HTMLCanvasElement;
