5131c3866d61bf4eb7cbf461a68d0e82fa86ad55
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-03 10:06:37 -0300

    docs: fazendo requisição get (net/http)

--- 006-alura-go-iniciando-com-a-linguagem-do-google/README.md
+++ 006-alura-go-iniciando-com-a-linguagem-do-google/README.md
@@ -5,3 +5,5 @@ docker exec -it [container] bash
 
 - Qualquer import ou declaração de variável que o código não utiliza, o compilador acusa e impede compilação (bem bolado)
 - Variáveis declaradas mas não atribuídas, recebem um valor inicial, dependendo do seu tipo (p. ex: int=0, string="", float32=0.0)
+
+- Pacotes padrão Go: https://pkg.go.dev/std
---
+++ 006-alura-go-iniciando-com-a-linguagem-do-google/src/aula4/aula4.go
@@ -0,0 +1,94 @@
+package main
+
+import "fmt"
+import "os"
+import "net/http"
+
+// import "reflect"
+
+func main() {
+	boasVindas()
+	exibeMenuDeOpcoes()
+	var opcaoEscolhida = pedeOpcaoDoUsuario()
+	processaOpcaoEscolhida(opcaoEscolhida)
+}
+
+func boasVindas() {
+	// Go possui inferência de tipos, ou seja,
+	// a partir da atribuição ela descobre o tipo, por exemplo:
+	// var nome = "conteúdo" (string)
+	// const idade = 22 (int)
+	// const versao = 1.1 (float64)
+	var nome = "Cezar"
+	const versao float32 = 0.1
+
+	// operador de declaração curta de variáveis:
+	// declara uma variável e infere seu tipo (a := "conteudo")
+	idade := 36
+
+	fmt.Println("Olá mundo e", nome, "que tem", idade, "anos, certo?")
+	fmt.Println("Versão do software:", versao)
+
+	// fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	// fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
+	// fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
+}
+
+func exibeMenuDeOpcoes() {
+	fmt.Println("####### Menu de opções ########")
+	fmt.Println("# 1 - Iniciar monitoramento   #")
+	fmt.Println("# 2 - Exibir logs             #")
+	fmt.Println("# 0 - Sair                    #")
+	fmt.Println("###############################")
+}
+
+func pedeOpcaoDoUsuario() int {
+	fmt.Print("Digite sua opção: ")
+	var comando int
+	// scan infere tipo da variável e restringe/formata entrada automaticamente
+	fmt.Scan(&comando)
+	//fmt.Println("Olha só onde ficou armazenada sua escolha:", &comando)
+
+	// outra forma de obter entrada (explicitando formato da entrada)
+	// fmt.Scanf("%d", &comando)
+	return comando
+}
+
+func processaOpcaoEscolhida(opcao int) {
+	fmt.Println("Você escolheu", opcao)
+
+	// if opcao == 1 {
+	// 	fmt.Println("Vamos fazer monitoramento...")
+	// } else if opcao == 2 {
+	// 	fmt.Println("Exibindo logs")
+	// } else if opcao == 0 {
+	// 	fmt.Println("Saindo, adeus.")
+	// } else {
+	// 	fmt.Println("Opção inválida.")
+	// }
+
+	switch opcao {
+	case 0:
+		fmt.Println("Saindo, adeus.")
+		os.Exit(0)
+		//fallthrough
+	case 1:
+		monitoramento()
+	case 2:
+		fmt.Println("Exibindo logs")
+	default:
+		fmt.Println("Opção inválida.")
+		os.Exit(1)
+	}
+
+}
+
+func monitoramento() {
+	fmt.Println("Iniciando monitoramento ...")
+
+	site := "https://www.cecez.com.br"
+	resposta, _ := http.Get(site)
+	fmt.Println(resposta)
+
+	fmt.Println("... terminando monitoramento.")
+}
