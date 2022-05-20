1a439ade712bb936836c058fe47e51c26aa5922d
Cezar Castro Rosa <cecez86@gmail.com>
2022-05-20 17:30:42 -0300

    docs: forEach, map, reduce

--- 010-alura-fundamentos-js-arrays/array.js
+++ 010-alura-fundamentos-js-arrays/array.js
@@ -1,6 +1,28 @@
 const notas = [1, 2, 3, 4];
 notas.push(5);
 ultimaNota = notas.pop();
-console.log(ultimaNota);
+//console.log(ultimaNota);
 
 // splice - emendar/juntar
+
+
+// forEach
+let soma = 0;
+notas.forEach(function(nota) {
+    soma += nota;
+});
+let media = soma / notas.length;
+console.log(notas);
+console.log(media);
+
+// map
+let novasNotas = notas.map(function(nota) {
+    return nota * 2;
+});
+console.log(novasNotas);
+
+// reduce
+let soma2 = notas.reduce(function(total, nota) {
+    return total + nota;
+}, 0);
+console.log(soma2);
