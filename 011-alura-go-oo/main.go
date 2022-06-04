05c287d652e138adf41dfc030920d2fed3e86af6
Cezar Rosa <cecez86@gmail.com>
2022-06-04 12:06:43 -0300

    docs: pointers

--- 011-alura-go-oo/main.go
+++ 011-alura-go-oo/main.go
@@ -10,6 +10,13 @@ type CheckingAccount struct {
 	balance float64
 }
 
+func (c *CheckingAccount) Withdraw(valueToWithdraw float64) {
+	canWithdraw := valueToWithdraw > 0 && valueToWithdraw <= c.balance
+	if canWithdraw {
+		c.balance -= valueToWithdraw
+	}
+}
+
 func main() {
 	// structure instantiation
 	cezarAccount := CheckingAccount{
@@ -19,6 +26,21 @@ func main() {
 	}
 	anaAccount := CheckingAccount{"Ana", 123, 457, 0.0}
 
+	// using pointer to structure
+	var joaoAccount *CheckingAccount
+	joaoAccount = new(CheckingAccount)
+	joaoAccount.holder = "João"
+	joaoAccount.agency = 321
+
 	fmt.Println(cezarAccount)
 	fmt.Println(anaAccount)
+	fmt.Println(joaoAccount)
+	fmt.Println(*joaoAccount)
+
+	// method using pointer to structure
+	cezarAccount.balance = 1000.0
+	cezarAccount.Withdraw(500.0)
+	fmt.Println(cezarAccount)
+	cezarAccount.Withdraw(1500.0)
+	fmt.Println(cezarAccount)
 }
