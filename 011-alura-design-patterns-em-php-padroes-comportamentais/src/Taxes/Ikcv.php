aa2216320e2d35fce9a1d169e6fc3994e316b575
Cezar Rosa <cecez86@gmail.com>
2022-06-19 11:36:21 -0300

    docs: template method example

---
+++ 011-alura-design-patterns-em-php-padroes-comportamentais/src/Taxes/Ikcv.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cezarcastrorosa\AluraDesignPatternsEmPhpPadroesComportamentais\Taxes;
+
+use Cezarcastrorosa\AluraDesignPatternsEmPhpPadroesComportamentais\Budget;
+
+class Ikcv extends TaxBase
+{
+    protected function shouldApplyTax(Budget $budget): bool
+    {
+        return $budget->value > 300 && $budget->amountOfItems > 10;
+    }
+
+    protected function applyTax(Budget $budget): float
+    {
+        return $budget->value * 0.05;
+    }
+
+    protected function applyOtherTaxes(Budget $budget): float
+    {
+        return $budget->value * 0.02;
+    }
+}
