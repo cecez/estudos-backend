512a98aa1aa696cf33b91fbf388c279539751468
Cezar Castro Rosa <cecez86@gmail.com>
2022-10-17 16:28:42 -0300

    docs: get, set, atributos e metodos privados

--- 021-alura-js-programacao-orientada-a-objetos/aula3/User.js
+++ 021-alura-js-programacao-orientada-a-objetos/aula3/User.js
@@ -1,12 +1,29 @@
 export default class User {
+  #nome;
+  #email;
+  #role;
+  #ativo;
+
   constructor(nome, email, role, ativo = true) {
-    this.nome = nome;
-    this.email = email;
-    this.role = role || "estudante";
-    this.ativo = ativo;
+    this.#nome = nome;
+    this.#email = email;
+    this.#role = role || "estudante";
+    this.#ativo = ativo;
+  }
+
+  get ativo() {
+    return this.#ativo;
+  }
+
+  set nome(novoNome) {
+    this.#nome = novoNome;
+  }
+
+  #metodoPrivado(argumento) {
+    console.log(`Logando ${argumento}`);
   }
 
   toString() {
-    return `${this.nome} - ${this.email}`;
+    return `${this.#nome} - ${this.#email}`;
   }
 }
--- 021-alura-js-programacao-orientada-a-objetos/readme.md
+++ 021-alura-js-programacao-orientada-a-objetos/readme.md
@@ -10,3 +10,26 @@ oo com js
 __proto__ é uma propriedade que todos os objetos têm e que aponta para o protótipo que foi definido para aquele objeto.
 
 prototype é uma propriedade da função que é definida como protótipo quando usamos new para criar novos objetos.
+
+```js
+class User {
+ _role = '';
+
+ // setter para atributo
+ set role(tipoRole) {
+   if (tipoRole !== 'admin') {
+     tipoRole = 'estudante'
+   }
+   this._role = tipoRole
+ }
+
+ // getter
+ get role() {
+   return this._role
+ }
+
+ constructor(nome) {
+   this._nome = nome;
+ }
+}
+```
